b6abec4335f49dd1020feeb18727ff092d926f58
Jose Campos Padilla <Jose Campos Padilla>
2017-01-16 23:36:20 -0300

    Perfilamiento del sistema tipo: usuario -> perfil -> funciones

--- php_action/core.php
+++ php_action/core.php
@@ -11,35 +11,6 @@ if(!$_SESSION['userId']) {
 }
 
 
-$profileId = '';
-$urlActual = '';
-$urlActual = $_SERVER['REQUEST_URI'];
-$profileId = $_SESSION['profile'];
-
-$tieneAcceso = false;
-
-$sqlProfile = "SELECT b.page_url FROM functions a inner join page b on a.page_id = b.page_id WHERE profile_id = $profileId;";
-$result = $connect->query($sqlProfile);
-
-while($row = $result->fetch_array()) {
-	$url = $row[0];
-
-	//echo "<script>console.log( 'Debug Objects: " .  . "' );</script>";
-	if (strpos($urlActual,$url) !== false) {
-		//echo "<script>console.log( 'Debug Objects: " . "SI" . "' );</script>";
-		$tieneAcceso = true;
-		
-	}
-
-
-}
-//echo "<script>console.log( 'Debug Objects: " . "NO" . "' );</script>";
-//   if (!$tieneAcceso){
-//  	//echo "<script>console.log( 'Debug Objects: " . "SI" . "' );</script>";
-//   	header('location: logout.php');
-//   } else {
-
-// }
 
 
 
