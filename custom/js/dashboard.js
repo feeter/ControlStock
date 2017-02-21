b429446174a8e77818aea09b41abdf42072dda42
Jose Campos Padilla <Jose Campos Padilla>
2017-02-21 01:52:45 -0300

    se muestra en el fullcandar el total de ganancias por dia

--- custom/js/dashboard.js
+++ custom/js/dashboard.js
@@ -21,10 +21,19 @@ $(document).ready(function() {
             //console.log(view.intervalStart.format());
             var res = view.intervalStart.format().split("-");
             nombreCalendario(res[1], res[0]);
+        },
+        dayRender: function(date, cell) {
+            //console.log("date: " + date.format() + " cell: " + cell);
+
+            var fecha = cell[0].getAttribute("data-date");
+
+            obtenerMontoDelDia(cell, fecha);
+
+            // if (moment().diff(date, 'days') > 0) {
+            //     cell.css("background-color", "silver");
+            // }
+
         }
-
-
-
     });
 
 
@@ -32,6 +41,40 @@ $(document).ready(function() {
 
 });
 
+//Ingresa el monto vendido en el plugin FullCalendar segun la fecha indicada en el parametro 
+function obtenerMontoDelDia(cell, fecha) {
+
+    var fechaArray = fecha.split("-");
+
+    var dia = fechaArray[2];
+    var mes = fechaArray[1];
+    var y = fechaArray[0];
+
+    $.ajax({
+        url: 'php_action/getTotalByDay.php',
+        type: 'post',
+        data: { day: dia, month: mes, year: y },
+        dataType: 'json',
+        success: function(response) {
+
+            if (response.sumaTotal !== null) {
+                cell[0].innerHTML = "<span class='label label-info'>$ " + response.sumaTotal + "</span>"
+            }
+
+
+        },
+        error: function(xhr, ajaxOptions, thrownError) {
+            console.log(xhr.status);
+            console.log(thrownError);
+
+        }
+
+
+    });
+
+
+
+}
 
 
 function nombreCalendario(mes, y) {
@@ -52,7 +95,7 @@ function nombreCalendario(mes, y) {
 
         }
 
-    }); // /fetch the selected categories data
+    });
 
 
 }
